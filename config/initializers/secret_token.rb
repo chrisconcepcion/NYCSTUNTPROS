86bcd776cc07fb2aa27732efdc68135e0554ea4b
Christopher Concepcion <feverdoingwork@gmail.com>
2014-03-11 17:35:16 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StuntRails::Application.config.secret_key_base = '6ce44a2b8df8668ea58e98656ba08f58a42077911d7dc0295406e92a39a4f3f8ce114bbc16fbfd941d81418884c0953ea8707572caff24b0c0dd2c627c7e3541'
